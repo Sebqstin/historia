64974b20750df43e4ab553bad3c36653f0aa8ee7
Sebqstin <110260631+Sebqstin@users.noreply.github.com>
2024-08-16 12:31:10 -0300

    dasd

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,171 +1,150 @@
 export const perguntas = [
     {
-        enunciado: "Depois dele assumir a presidência do país, tive uma migração massiva dos seus moradores, as pessoas não suportavam ficar lá em essas condições.",
+        enunciado: "Após Nicolás Maduro assumir a presidência da Venezuela em 2014, a situação no país piorou drasticamente, resultando em uma migração em massa de seus moradores. As pessoas não conseguiam suportar as condições lá. Como você reage a essa situação?",
         alternativas: [
             {
-                texto: "Grave",
+                texto: "Triste",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                    "Ficou chocado com a rapidez com que a crise afetou a vida das pessoas.",
+                    "Achou assustador como a situação do país se deteriorou tão rapidamente."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso pois vou ficar no país que eu amo",
+                texto: "Esperançoso",
                 afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                    "Quis explorar como a tecnologia pode ajudar em situações de crise.",
+                    "Pensou que, mesmo em tempos difíceis, a tecnologia pode oferecer soluções."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Você, como não tem o dinheiro suficiente, resolve encontrar um emprego para também migrar. Após você achar o emprego, consigue juntar o dinheiro suficiente.",
+        enunciado: "Como você não tem o dinheiro suficiente para emigrar, decide encontrar um emprego na Venezuela e juntar dinheiro para se mudar. Depois de conseguir o emprego e reunir o dinheiro necessário, o que você faz?",
         alternativas: [
             {
-                texto: "Escolhe ficar no pais e Empreender",
+                texto: "Escolhe ficar no país e Empreender",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "Conseguiu usar a tecnologia para buscar informações úteis sobre como empreender.",
+                    "Percebeu que a tecnologia pode ajudar a encontrar informações valiosas de forma rápida e eficaz.",
+                    "Notou que a tecnologia pode explicar conceitos complexos de maneira simples, o que ajudou muito nas suas pesquisas."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Toma uma coragem e vai na aventura de migrar para outro pais que você não conhece",
+                texto: "Decide migrar para outro país em busca de novas oportunidades",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para se adaptar ao novo ambiente.",
+                    "Achou que era mais eficaz procurar respostas usando métodos tradicionais, apesar de levar mais tempo.",
+                    "Sentiu um pouco de receio sobre como a tecnologia pode manipular dados pessoais e preferiu manter uma abordagem mais conservadora."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Uma professora de tecnologia da escola decide fazer uma sequência de aulas sobre a aplicação de tecnologias em situações de crise. No fim de uma aula, ela pede que você escreva um trabalho sobre como a tecnologia pode ajudar em crises. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Usa uma ferramenta de busca na internet que utiliza tecnologia avançada para encontrar informações relevantes e explica de forma clara.",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "Conseguiu utilizar a tecnologia para encontrar informações úteis sobre a crise.",
+                    "Percebeu que a tecnologia pode ajudar a acessar informações rapidamente e com foco.",
+                    "Notou que a tecnologia facilita a compreensão de conceitos complexos, o que ajudou bastante na sua pesquisa."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "Escreve o trabalho com base nas conversas com colegas, algumas pesquisas na internet e conhecimentos pessoais sobre o tema.",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                    "Sentiu mais confiança em usar seus próprios recursos e conhecimento para completar o trabalho.",
+                    "Achou que era mais fácil usar métodos tradicionais para obter informações, mesmo que fosse mais demorado.",
+                    "Sentiu um pouco de preocupação com o uso da tecnologia para processar dados pessoais e optou por métodos mais manuais."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "O empreendimento está dando certo, consegue clientes, tem boas vendas e o teu salário da para viver bem, mas pela corrupção do governo, o pais se encontra numa crise empresária e não há fornecedores para o teu empreendimento",
+        enunciado: "Seu empreendimento está indo bem, você tem clientes e boas vendas, mas devido à crise econômica provocada pela corrupção do governo, o país está enfrentando uma grave crise de fornecedores. Como você reage?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Inicia inovações na área tecnológica e busca novas oportunidades profissionais.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Foca na inovação tecnológica para superar a crise e criar novas oportunidades.",
+                    "Participa ativamente no desenvolvimento de soluções criativas e na melhoria de processos tecnológicos."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Preocupa-se com a perda de empregos devido à automação e defende a proteção dos trabalhadores.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "Sua preocupação levou à criação de grupos de estudo sobre o uso ético da tecnologia.",
+                    "Participa de iniciativas que visam reduzir as desigualdades geradas pela automação e proteger os trabalhadores."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "Você deve participar de um debate sobre como a tecnologia impacta o trabalho do futuro. Qual é o seu posicionamento?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Defende que a tecnologia pode criar novas oportunidades de emprego e melhorar as habilidades humanas.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Acredita que a tecnologia pode impulsionar a inovação e abrir novas oportunidades profissionais.",
+                    "Atua no desenvolvimento de soluções criativas e na melhoria de processos tecnológicos."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Preocupa-se com a possibilidade de perda de empregos para máquinas e defende a proteção dos trabalhadores.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "Sua preocupação resultou na criação de grupos de estudo para discutir o uso ético da tecnologia.",
+                    "Trabalha em iniciativas para reduzir as desigualdades e proteger os trabalhadores afetados pela automação."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após um debate em sala de aula sobre o impacto da tecnologia no futuro do trabalho, qual é o seu posicionamento sobre o impacto da tecnologia no mercado de trabalho?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Defende que a tecnologia pode criar novas oportunidades de emprego e melhorar as habilidades humanas.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Acredita que a tecnologia promove inovação e novas oportunidades de carreira.",
+                    "Participa ativamente no desenvolvimento de soluções tecnológicas e na melhoria de processos."
                 ],
                 proxima: 7,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Preocupa-se com a perda de empregos para máquinas e defende a proteção dos trabalhadores.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "Cria grupos para discutir o uso ético da tecnologia e proteger os trabalhadores.",
+                    "Trabalha para mitigar as desigualdades causadas pela automação e proteger os empregos."
                 ],
                 proxima: 7,
             },
         ]
     },
     {
-        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Ao final do debate, você precisa criar uma imagem no computador que represente suas opiniões sobre a tecnologia. Como você procede?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Cria uma imagem usando uma plataforma de design tradicional como o Paint.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                    "Percebeu que muitas pessoas ainda têm dificuldades com ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design.",
+                    "Acredita que métodos tradicionais de desenho são mais eficazes e incentiva outros a explorar técnicas clássicas como a pintura em aquarela."
                 ],
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                texto: "Usa um gerador de imagens baseado em tecnologia avançada para criar a imagem.",
                 afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                    "Usa geradores de imagens para acelerar o processo criativo e ensinar outras pessoas a utilizar essas ferramentas.",
+                    "Compartilha suas criações nas redes sociais para ensinar como se comunicar artisticamente através da tecnologia.",
+                    "Reconhece que muitas pessoas têm dificuldade em desenhar e acredita que a tecnologia pode ajudá-las a expressar suas ideias."
                 ],
             },
         ]
